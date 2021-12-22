ca67d54af15ee0f1de2082db7b3b77bdaa83c750
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2021-12-22 14:10:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+(function () {
+  let count = 0;
+  const value = document.getElementById("value");
+  const btns = document.querySelectorAll(".btn");
+  btns.forEach(function (btn) {
+    btn.addEventListener("click", (event) => {
+      const styles = event.target.classList;
+      if (styles.contains("decrease")) {
+        count--;
+      } else if (styles.contains("increase")) {
+        count++;
+      } else {
+        count = 0;
+      }
+      if (count > 0) {
+        value.style.color = "green";
+      } else if (count < 0) {
+        value.style.color = "red";
+      } else {
+        value.style.color = "black";
+      }
+      value.innerText = `${count}`;
+    });
+  });
+})();
